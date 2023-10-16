bb67cf1c7b03c5152a27a36a45fa876d324b696d
ffuf-yoki <enrico.cuison@ffufm.com>
2023-10-16 15:38:45 -0400

    [AUTH-2] GitHub Lint Workflow
    - add lint check
    - add temporary lint to test if workflow works

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      /
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
